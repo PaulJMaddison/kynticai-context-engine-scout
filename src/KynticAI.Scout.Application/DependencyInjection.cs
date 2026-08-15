a81262e3729a7779b4cc4ebd99eb88a93d8991cd
Paul Maddison <paul.maddison.delimeg@gmail.com>
2026-08-15 21:04:44 +0100

    Register Scout upgrade compatibility service

--- src/KynticAI.Scout.Application/DependencyInjection.cs
+++ src/KynticAI.Scout.Application/DependencyInjection.cs
@@ -11,6 +11,7 @@ public static class DependencyInjection
     public static IServiceCollection AddScoutApplication(this IServiceCollection services)
     {
         services.AddScoped<IScoutService, ScoutService>();
+        services.AddScoped<IScoutUpgradeCompatibilityService, ScoutUpgradeCompatibilityService>();
         services.AddScoped<BasicRelationshipEngine>();
         services.AddScoped<EnterpriseRelationshipEngineHandoff>();
         services.AddScoped<INextActionIntelligenceService, NextActionIntelligenceService>();
