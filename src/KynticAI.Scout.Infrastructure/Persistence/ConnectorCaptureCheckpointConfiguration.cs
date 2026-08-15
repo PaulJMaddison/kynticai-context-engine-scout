1e6789123228df3aded83ec219ae322458356a38
Paul Maddison <paul.maddison.delimeg@gmail.com>
2026-08-15 23:21:51 +0100

    Persist capture payload storage contract

--- src/KynticAI.Scout.Infrastructure/Persistence/ConnectorCaptureCheckpointConfiguration.cs
+++ src/KynticAI.Scout.Infrastructure/Persistence/ConnectorCaptureCheckpointConfiguration.cs
@@ -14,6 +14,7 @@ internal sealed class ConnectorCaptureCheckpointConfiguration : IEntityTypeConfi
         builder.Property(x => x.CaptureProfileVersion).HasMaxLength(80).IsRequired();
         builder.Property(x => x.CoverageScope).HasMaxLength(80).IsRequired();
         builder.Property(x => x.HistoryCompleteness).HasMaxLength(80).IsRequired();
+        builder.Property(x => x.PayloadStorageContract).HasMaxLength(80).IsRequired();
         builder.Property(x => x.ContinuationToken).HasMaxLength(8_000);
         builder.Property(x => x.HighWaterMarkJson).HasColumnType("jsonb").IsRequired();
         builder.Property(x => x.LeaseOwner).HasMaxLength(200).IsConcurrencyToken();
