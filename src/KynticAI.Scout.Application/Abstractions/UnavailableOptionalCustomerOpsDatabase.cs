7e174afe6aa1f397f294ed3bb7c98c9c39878d1e
Paul Maddison <paul.maddison.delimeg@gmail.com>
2026-08-27 18:42:11 +0100

    refactor: remove reference-only implementation from Scout core

--- src/KynticAI.Scout.Application/Abstractions/UnavailableOptionalCustomerOpsDatabase.cs
+++
@@ -1,7 +0,0 @@
-namespace KynticAI.Scout.Application.Abstractions;
-
-/// <summary>Default absence value for the LocalDemo-only reference store.</summary>
-public sealed class UnavailableOptionalCustomerOpsDatabase : IOptionalCustomerOpsDatabase
-{
-    public System.Data.Common.DbConnection? Connection => null;
-}
